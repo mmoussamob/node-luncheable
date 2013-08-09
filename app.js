ab4429d82afb7c2e2e1a843b1b3085baaba946a3
mmoussamob <mmoussa@mobiquityinc.com>
2013-08-09 13:02:32 -0400

    add bookshelf

--- app.js
+++ app.js
@@ -27,7 +27,10 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function(req, res) {
+  res.render('books/bookshelf');
+});
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
